fc15a92e9960fbfdac25b3d78a43e61c5f21cc74
Barbara Perlik <barbperlik@gmail.com>
2016-12-11 13:42:36 -0500

    add controllers for landing and collection views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,30 @@
 (function () {
 	function config($stateProvider, $locationProvider) {
 		$locationProvider
-         .html5Mode({
-             enabled: true,   /* disables hashbang URLS */
-             requireBase: false /* one way to avoid a common location error */
-         });
-		 $stateProvider
-         .state('landing', {
-             url: '/',
-             templateUrl: '/templates/landing.html'
+			.html5Mode({
+				enabled: true,
+				/* disables hashbang URLS */
+				requireBase: false /* one way to avoid a common location error */
+			});
+		$stateProvider
+			.state('landing', {
+				url: '/',
+				controller: 'LandingCtrl as landing',
+				templateUrl: '/templates/landing.html'
 			})
-         .state('album', {
-             url: '/album',
-             templateUrl: '/templates/album.html'
-		 	})
-		   .state('collection', {
-			    url: '/collection',
-			 	 templateUrl:'/templates/collection.html'
-         });	
-		
+			.state('album', {
+				url: '/album',
+				templateUrl: '/templates/album.html'
+			})
+			.state('collection', {
+				url: '/collection',
+				controller: 'CollectionCtrl as collection',
+				templateUrl: '/templates/collection.html'
+			});
+
 	}
 	angular
 		.module('blocJams', ['ui.router'])
-/*To make sure the providers are accessible throughout the application, inject them using the config block on the application's root module. Write a config function to pass into the config() function: */
+		/*To make sure the providers are accessible throughout the application, inject them using the config block on the application's root module. Write a config function to pass into the config() function: */
 		.config(config);
-})();                              
-
+})();
