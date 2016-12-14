61b6e7ffd2d1fd2a0fad269c52f5a5fca14e799f
Barbara Perlik <barbperlik@gmail.com>
2016-12-13 19:25:04 -0500

    Add albumcontroller for song data

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,7 @@
 (function () {
-	function AlbumCtrl() {	
-		   this.songs = [];
-			for (var i=1; i< album.songs.length; i++) {
-				this.albums.push(angular.copy(albumPicasso));
-			}
+	function AlbumCtrl() {			
+		this.albumData = albumPicasso;
+		
 	}
 
 	angular
